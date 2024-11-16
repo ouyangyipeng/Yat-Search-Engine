ecd979328527260e0f220b9104434d315771a834
Fernández Owen <151031903+ouyangyipeng@users.noreply.github.com>
2024-11-16 04:03:24 +0000

    try to use cmake1

---
+++ build/CMakeFiles/TextSearchEngine.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TextSearchEngine.dir/src/RegexSearch.cpp.o"
+  "CMakeFiles/TextSearchEngine.dir/src/TextSearchEngine.cpp.o"
+  "CMakeFiles/TextSearchEngine.dir/src/main.cpp.o"
+  "TextSearchEngine"
+  "TextSearchEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TextSearchEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
